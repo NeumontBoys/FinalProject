1e0806f801ad398e3a0ce5159d46c202b8280102
Aislin-Spencer <98478490+Aislin-Spencer@users.noreply.github.com>
2022-02-28 18:46:41 -0700

    Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+package edu.neumont.oop;
+
+import edu.neumont.oop.Controller.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+    }
+}
