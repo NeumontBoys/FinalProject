058e136be42b63547c97ed8d127c1c77d482c444
Aislin-Spencer <98478490+Aislin-Spencer@users.noreply.github.com>
2022-02-28 18:27:37 -0700

    WIP Menu
    Please let me know when you have figured out which races and classes you wish to create. Also, I just made 10 sections for races/classes, but don't feel that we need that many.

--- Menu.java
+++ Menu.java
@@ -20,7 +20,13 @@ public class Menu {
             case 1: character.setPersonRace("Elf");
             case 2: character.setPersonRace("Dwarf");
             case 3: character.setPersonRace("Halfling");
-            //...
+            case 4: character.setPersonRace("");
+            case 5: character.setPersonRace("");
+            case 6: character.setPersonRace("");
+            case 7: character.setPersonRace("");
+            case 8: character.setPersonRace("");
+            case 9: character.setPersonRace("");
+            case 10: character.setPersonRace("");
         }
     }
 
@@ -34,7 +40,13 @@ public class Menu {
             case 1: character.setPersonClass("Warrior");
             case 2: character.setPersonClass("Mage");
             case 3: character.setPersonClass("Rogue");
-            //...
+            case 4: character.setPersonClass("");
+            case 5: character.setPersonClass("");
+            case 6: character.setPersonClass("");
+            case 7: character.setPersonClass("");
+            case 8: character.setPersonClass("");
+            case 9: character.setPersonClass("");
+            case 10: character.setPersonClass("");
         }
     }
 
