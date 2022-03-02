4a2f1fd0ea26b006862869d4b67b16d5d9a14af8
matthancantrell <66035237+matthancantrell@users.noreply.github.com>
2022-03-01 19:02:19 -0700

    Implimenting ConsoleIO.promptForMenuSelection()
    The multi-line strings could be simplified down to one line by using the ConsoleIO class from [8-2] and [8-3]. The library jar has been put in main if needed.

--- Menu.java
+++ Menu.java
@@ -10,73 +10,26 @@ public class Menu {
 
     public void start(){
         //Start with character creation
-        //Prompt for character name?
+        nameSelect();
         raceSelect();
         classSelect();
         game.startGame();
     }
 
-    private void raceSelect() {
-        int selection = 0; //Get user input for number - Provide prompt
-        switch (selection){
-            case 1: character.setPersonRace("Elf");
-            case 2: character.setPersonRace("Dwarf");
-            case 3: character.setPersonRace("Halfling");
-            case 4: character.setPersonRace("");
-            case 5: character.setPersonRace("");
-            case 6: character.setPersonRace("");
-            case 7: character.setPersonRace("");
-            case 8: character.setPersonRace("");
-            case 9: character.setPersonRace("");
-            case 10: character.setPersonRace("");
-        }
+    private void nameSelect() {
+        String name = ConsoleIO.promptForString("Enter Your Character's Name: ", false);
+        character.setName(name);
     }
 
-    private String racePrompt(){
-        return """
-                Please select a race from the following options:
-                1) Elf
-                2) Dwarf
-                3) Halfling
-                4) 
-                5) 
-                6) 
-                7) 
-                8) 
-                9) 
-                10)
-                """;
+    private void raceSelect() {
+        System.out.println("Please Select A Race For Your Character: ");
+        int select = ConsoleIO.promptForMenuSelection(character.getRaces(), false);
+        character.setRaceValue(select);
     }
 
     private void classSelect(){
-        int selection = 0; //Get user input for number - Provide prompt
-        switch (selection){
-            case 1: character.setPersonClass("Rogue");
-            case 2: character.setPersonClass("Mage");
-            case 3: character.setPersonClass("Warrior");
-            case 4: character.setPersonClass("");
-            case 5: character.setPersonClass("");
-            case 6: character.setPersonClass("");
-            case 7: character.setPersonClass("");
-            case 8: character.setPersonClass("");
-            case 9: character.setPersonClass("");
-            case 10: character.setPersonClass("");
-        }
-    }
-
-    private String classPrompt(){
-        return """
-                Please select a class from the following options:
-                1) Rogue
-                2) Mage
-                3) Warrior
-                4) 
-                5) 
-                6) 
-                7) 
-                8) 
-                9) 
-                10)
-                """;
+        System.out.println("Please Select A Class For Your Character: ");
+        int select = ConsoleIO.promptForMenuSelection(character.getClasses(), false);
+        character.setClassValue(select);
     }
 }
