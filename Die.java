64317db60758c309b57765333fb2e632df21fe5d
Aislin-Spencer <98478490+Aislin-Spencer@users.noreply.github.com>
2022-03-02 18:43:21 -0700

    Update Die.java
    When adjusting code that wasn't yours to begin with, please make a new branch and start a pull request instead of making a direct commit. Please and thank you!

--- Die.java
+++ Die.java
@@ -1,54 +1,37 @@
 package edu.neumont.oop.Controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import lib.ConsoleIO;
+
 import java.util.Random;
 
 public class Die {
-    //Properties
+    //Called Classes
     private final Random random = new Random();
-    private final BufferedReader inputSides = new BufferedReader(new InputStreamReader(System.in));
-    private int roll;
-
-    //Getter & Setter
-    public int getRoll() {
-        return roll;
-    }
-
-    public void setRoll(int roll) {
-        this.roll = roll;
-    }
 
     //Roll methods
     public int rollOnce(int sides){
+        //Validate input
         while(sides < 4 || sides > 20){
-            System.out.println("Please insert a valid number of sides: 4-20");
-            sides = Integer.parseInt(String.valueOf(inputSides));
+            sides = ConsoleIO.promptForInt("Please insert a valid number of sides: 4-20", 4, 20);
         }
         //Generates random number
         int dieSides = sides + 1;
-        setRoll(random.nextInt(dieSides) - 1);
-        return getRoll();
+        return random.nextInt(dieSides) - 1;
     }
 
     public int rollMultiple(int dieCount, int sides){
+        //Validate input
         if(dieCount < 1 || dieCount > 10){
-            System.out.println("Please enter a valid number of dice: 1-10");
+            dieCount = ConsoleIO.promptForInt("Please enter a valid number of dice: 1-10", 1, 10);
         }
         //Loop that calls rollOnce() for each die
         int totalRoll = 0;
         for(int dice = 0; dice < dieCount; dice++) {
-            setRoll(rollOnce(sides));
-            totalRoll += getRoll();
+            totalRoll += rollOnce(sides);
         }
         return totalRoll;
     }
-    
-    // Can roll singular or multiple dice
-       public int rollDice(int numOfDice, int numOfSides){
-        return numOfDice * randomGenerator(1, numOfSides);
-    }
-    
+
     //Random Number Generator
     public int randomGenerator (int lowBound, int upBound){
         int randomNumber = random.nextInt((upBound - lowBound) + 1) + lowBound;
