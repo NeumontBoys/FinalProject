8fe2b50c2d789ebd6f118dffaef7ce511b6b8bbe
Aislin-Spencer <98478490+Aislin-Spencer@users.noreply.github.com>
2022-03-02 20:31:39 -0700

    Update MonsterFactory.java

--- MonsterFactory.java
+++ MonsterFactory.java
@@ -1,181 +1,201 @@
-public class MonsterFactory {
+package edu.neumont.oop.Model;
 
+import edu.neumont.oop.Controller.Die;
+
+public class MonsterFactory {
+    //Called Class
+    Die die = new Die();
+
+    //Arrays
     private final Monster[] monsters = {skeleton(), slime(), zombie(), troll(), ogre(), wisp(), amalgamate(), bat(), mimic(), mummy(), ghost(), goblin(), hobgoblin(), bugBear(), construct()};
     private final Monster[] miniBosses = {champMinotaur(), giantFuckinRat(), banshee()};
     private final Monster[] bosses = {theDuck(), shadowBrute(), lich()};
 
-    // 15 monsters, 3 mini bosses, 3 bosses
-    /////////////////////////////////////////////////////////// Matthan's Creatures Below
+    // 15 monsters
+    //-----Matthan's Monsters-----//
     public Monster skeleton(){
-        if(d!= null){
-            InteractPerson skeleton = new Monster("Skeleton");
-            skeleton.setHp(d.randomGenerator(5, 8));
-            skeleton.setArmorClass(d.randomGenerator(3, 5));
-            return (Monster) skeleton;
+        if(die != null){
+            int hp = die.randomGenerator(5, 8);
+            int ac = die.randomGenerator(3, 5);
+            return new Monster("Skeleton", hp, ac);
         }
         return null;
-    } // DONE
-    public Monster slime(){
-        if(d!=null){
-            InteractPerson slime = new Monster("Slime");
-            slime.setHp(d.randomGenerator(3, 12));
-            slime.setArmorClass(d.randomGenerator(1, 3));
-            return (Monster) slime;
-        }
-        return null;
-    } // DONE
-    public Monster zombie(){
-    if(d!=null){
-        InteractPerson zombie = new Monster("Zombie");
-        zombie.setHp(d.randomGenerator(5, 15));
-        zombie.setArmorClass(d.randomGenerator(5, 10));
-        return (Monster) zombie;
     }
-    return null;
-    } // DONE
-    public Monster troll(){
-        if(d!=null){
-            InteractPerson troll = new Monster("Troll");
-            troll.setHp(d.randomGenerator(20, 30));
-            troll.setArmorClass(d.randomGenerator(7, 13));
-            return (Monster) troll;
+    public Monster slime(){
+        if(die != null){
+            int hp = die.randomGenerator(3, 12);
+            int ac = die.randomGenerator(1, 3);
+            return new Monster("Slime", hp, ac);
         }
         return null;
-    } // DONE
+    }
+    public Monster zombie(){
+        if(die != null){
+            int hp = die.randomGenerator(5, 15);
+            int ac = die.randomGenerator(5, 10);
+            return new Monster("Zombie", hp, ac);
+        }
+        return null;
+    }
+    public Monster troll(){
+        if(die != null){
+            int hp = die.randomGenerator(20, 30);
+            int ac = die.randomGenerator(7, 13);
+            return new Monster("Troll", hp, ac);
+        }
+        return null;
+    }
     public Monster ogre(){
-        if(d!=null){
-            InteractPerson ogre = new Monster("Ogre");
-            ogre.setHp(d.randomGenerator(15, 18));
-            ogre.setArmorClass(d.randomGenerator(10, 13));
-            return (Monster) ogre;
+        if(die != null){
+            int hp = die.randomGenerator(15, 18);
+            int ac = die.randomGenerator(10, 13);
+            return new Monster("Ogre", hp, ac);
         }
         return null;
 
-    } // DONE
-    public Monster champMinotaur(){
-        if(d!=null){
-            InteractPerson champMinotaur = new Monster("Champion Minotaur");
-            champMinotaur.setHp(d.randomGenerator(40,55));
-            champMinotaur.setArmorClass(d.randomGenerator(12,16));
-            return (Monster) champMinotaur;
-        }
-        return null;
     }
-    public Monster theDuck(){
-        if(d!=null){
-            InteractPerson theDuck = new Monster("The Duck");
-            theDuck.setHp(d.randomGenerator(75, 100));
-            theDuck.setArmorClass(d.randomGenerator(15, 18));
-            return (Monster) theDuck;
-        }
-        return null;
-    }
-    /////////////////////////////////////////////////////////// Matthan's Creatures Above
-    /////////////////////////////////////////////////////////// Mabel's Creatures Below
+    //-----Mabel's Monsters-----//
     public Monster wisp(){
-        if(d!=null){
-            InteractPerson wisp = new Monster("Wisp");
-            return (Monster) wisp;
+        if(die != null){
+            int hp = die.randomGenerator(0, 0);
+            int ac = die.randomGenerator(0, 0);
+            return new Monster("Wisp", hp, ac);
         }
         return null;
     }
     public Monster amalgamate(){
-        if(d!=null){
-            InteractPerson amalgamate = new Monster("Amalgamate");
-            return (Monster) amalgamate;
+        if(die != null){
+            int hp = die.randomGenerator(0, 0);
+            int ac = die.randomGenerator(0, 0);
+            return new Monster("Amalgamate", hp, ac);
         }
         return null;
     }
     public Monster bat(){
-        if(d!=null){
-            InteractPerson bat = new Monster("Bat");
-            return (Monster) bat;
+        if(die != null){
+            int hp = die.randomGenerator(0, 0);
+            int ac = die.randomGenerator(0, 0);
+            return new Monster("Bat", hp, ac);
         }
         return null;
     }
     public Monster mimic(){
-        if(d!=null){
-            InteractPerson mimic = new Monster("Mimic");
-            return (Monster) mimic;
+        if(die != null){
+            int hp = die.randomGenerator(0, 0);
+            int ac = die.randomGenerator(0, 0);
+            return new Monster("Mimic", hp, ac);
         }
         return null;
     }
     public Monster mummy(){
-        if(d!=null){
-            InteractPerson mummy = new Monster("Mummy");
-            return (Monster) mummy;
-        }
-        return null;
-        
-    }
-    public Monster giantFuckinRat(){
-        if(d!=null){
-            InteractPerson giantRat = new Monster("The Rat King");
-            return (Monster) giantRat;
-        }
-        return null;
-        
-    } // DONE
-    public Monster shadowBrute(){
-        if(d!=null){
-            InteractPerson shadowBrute = new Monster();
-            return (Monster) shadowBrute;
+        if(die != null){
+            int hp = die.randomGenerator(0, 0);
+            int ac = die.randomGenerator(0, 0);
+            return new Monster("Mummy", hp, ac);
         }
         return null;
     }
-    /////////////////////////////////////////////////////////// Mabel's Creatures Above
-    /////////////////////////////////////////////////////////// Aislin's Creatures Below
+    //-----Aislin's Monsters-----//
     public Monster ghost(){
-        if(d!=null){
-            InteractPerson ghost = new Monster("Ghost");
-            return (Monster) ghost;
+        if(die != null){
+            int hp = die.randomGenerator(10, 12);
+            int ac = die.randomGenerator(5, 8);
+            return new Monster("Ghost", hp, ac);
         }
         return null;
     }
     public Monster goblin(){
-        if(d!=null){
-            InteractPerson goblin = new Monster("Goblin");
-            return (Monster) goblin;
+        if(die != null){
+            int hp = die.randomGenerator(2, 6);
+            int ac = die.randomGenerator(2, 4);
+            return new Monster("Goblin", hp, ac);
         }
         return null;
     }
     public Monster hobgoblin(){
-        if(d!=null){
-            InteractPerson hobgoblin = new Monster("Hobgoblin");
-            return (Monster) hobgoblin;
+        if(die != null){
+            int hp = die.randomGenerator(6, 10);
+            int ac = die.randomGenerator(4, 6);
+            return new Monster("Hobgoblin", hp, ac);
         }
         return null;
     }
     public Monster bugBear(){
-        if(d!=null){
-            InteractPerson bugBear = new Monster("Bug Bear");
-            return (Monster) bugBear;
+        if(die != null){
+            int hp = die.randomGenerator(10, 15);
+            int ac = die.randomGenerator(8, 14);
+            return new Monster("Bug Bear", hp, ac);
         }
         return null;
     }
     public Monster construct(){
-        if(d!=null){
-            InteractPerson construct = new Monster("Construct");
-            return (Monster) construct;
+        if(die != null){
+            int hp = die.randomGenerator(20, 23);
+            int ac = die.randomGenerator(13, 16);
+            return new Monster("Construct", hp, ac);
         }
         return null;
     }
+    //--------------------------------------------------
+    // 3 mini bosses
+    //-----Matthan's Mini-Boss-----//
+    public Monster champMinotaur(){
+        if(die != null){
+            int hp = die.randomGenerator(40,55);
+            int ac = die.randomGenerator(12,16);
+            return new Monster("Champion Minotaur", hp, ac);
+        }
+        return null;
+    }
+    //-----Mabel's Mini-Boss-----//
+    public Monster giantFuckinRat(){
+        if(die != null){
+            int hp = die.randomGenerator(0, 0);
+            int ac = die.randomGenerator(0, 0);
+            return new Monster("The Rat King", hp, ac);
+        }
+        return null;
+    }
+    //-----Aislin's Mini-Boss-----//
     public Monster banshee(){
-        if(d!=null){
-            InteractPerson banshee = new Monster();
-            return (Monster) banshee;
+        if(die != null){
+            int hp = die.randomGenerator(30, 40);
+            int ac = die.randomGenerator(14, 16);
+            return new Monster("Banshee", hp, ac);
         }
         return null;
     }
+    //--------------------------------------------------
+    // 3 boss
+    //-----Matthan's Boss-----//
+    public Monster theDuck(){
+        if(die != null){
+            int hp = die.randomGenerator(75, 100);
+            int ac = die.randomGenerator(15, 18);
+            return new Monster("The Duck", hp, ac);
+        }
+        return null;
+    }
+    //-----Mabel's Boss-----//
+    public Monster shadowBrute(){
+        if(die != null){
+            int hp = die.randomGenerator(0, 0);
+            int ac = die.randomGenerator(0, 0);
+            return new Monster("Shadow Brute", hp, ac);
+        }
+        return null;
+    }
+    //-----Aislin's Boss-----//
     public Monster lich(){
-        if(d!=null){
-            InteractPerson lich = new Monster();
-            return (Monster) lich;
+        if(die != null){
+            int hp = die.randomGenerator(80, 100);
+            int ac = die.randomGenerator(16, 18);
+            return new Monster("Lich", hp, ac);
         }
         return null;
     }
-    /////////////////////////////////////////////////////////// Aislin's Creatures Above
 
+    //Get Monsters
     public Monster[] getMonsters() {
         return monsters;
     }
